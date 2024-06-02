9b954b362cffca586fb2e89414cfc427c5660bcc
tenda <jianzuoteng558@gmail.com>
2024-06-02 21:33:57 +0900

    掲示板の作成（管理者なし）

---
+++ database/migrations/2024_06_02_205241_create_posts_table.php
@@ -0,0 +1,26 @@
+<?php
+
+// database/migrations/xxxx_xx_xx_create_posts_table.php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
+
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,20 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\http\Controllers\Testcontroller;
 
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\PostController;
+
+
+Route::post('/posts/{post}/comments', [CommentController::class, 'store'])->name('comments.store');
+Route::get('/comments/{comment}/edit', [CommentController::class, 'edit'])->name('comments.edit');
+Route::put('/comments/{comment}', [CommentController::class, 'update'])->name('comments.update');
+Route::delete('/comments/{comment}', [CommentController::class, 'destroy'])->name('comments.destroy');
+
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+
+
+
 Route::get('/', function () {
     return view('welcome');
 });
